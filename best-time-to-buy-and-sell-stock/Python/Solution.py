03af6af6df007302e83ea28319ed518860f0cbc3
willychen0146 <willychen0146@gmail.com>
2024-03-24 18:49:23 +0800

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/

---
+++ best-time-to-buy-and-sell-stock/Python/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l = 0
+        r = 1
+        profit = 0
+        while(r < len(prices)):
+            if(prices[r] > prices[l]):
+                profit = max(prices[r]-prices[l], profit)
+            else:
+                l=r
+            r+=1
+        return profit
