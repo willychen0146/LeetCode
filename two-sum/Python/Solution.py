271ff458878fce0ffcc1d89871f12f806194ffc9
willychen0146 <willychen0146@gmail.com>
2024-03-24 18:52:38 +0800

    https://leetcode.com/problems/two-sum/description/

---
+++ two-sum/Python/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if(diff in dic):
+                return (dic[diff], i)
+            else:
+                dic[n] = i
