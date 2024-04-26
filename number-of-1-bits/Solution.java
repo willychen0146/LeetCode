9b110073e1c44b27749efdf237f293d615608ea8
willychen0146 <willychen0146@gmail.com>
2024-04-27 02:10:12 +0800

    https://leetcode.com/problems/number-of-1-bits/description/

---
+++ number-of-1-bits/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int result = 0;
+        while(n != 0){
+            n &= n-1;
+            result++;
+        }
+        return result;
+    }
+}
