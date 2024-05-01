c44a8cc37b56b4660cbc54dabd4f30cd8579dc76
willychen0146 <willychen0146@gmail.com>
2024-05-02 01:31:17 +0800

    https://leetcode.com/problems/insert-interval/description/

---
+++ insert-interval/Solution.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+
+        ArrayList<int[]> result = new ArrayList<>();
+
+        for(int[] interval : intervals){
+            if(newInterval == null || newInterval[0] > interval[1])
+                result.add(interval);
+            else if(newInterval[1] < interval[0]){
+                result.add(newInterval);
+                result.add(interval);
+                newInterval = null;
+            }
+            else{
+                newInterval[0] = Math.min(newInterval[0], interval[0]);
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            }
+        }
+        if(newInterval != null) result.add(newInterval);
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
